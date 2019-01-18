8cf7afa1f381cdcc1451662dcb4718cad5374939
Dejan Toteff <deyan8284@gmail.com>
2019-01-18 21:43:30 +0200

    feat: list imported colors ant

---
+++ src/ants/listImportedColors.js
@@ -0,0 +1,18 @@
+import importedColors from '../../colors.json'
+import { filter } from 'rambdax'
+
+export function listImportedColorsAnt(){
+  const keys = Object.keys(importedColors)
+  const holder = []
+  keys.forEach(key => {
+    const filtered = filter(
+      x => x.length > 0
+    )(importedColors[key])
+    const len = Object.keys(filtered).length
+    if(!len) return  
+    holder.push(
+      `${key} - ${len - 1}`
+    )
+  })
+  return holder
+}
---
+++ src/ants/listImportedColors.spec.js
@@ -0,0 +1,6 @@
+import {listImportedColorsAnt} from './listImportedColors'
+
+test('showImportedColors', () => {
+  const result = listImportedColorsAnt()
+  expect(Array.isArray(result)).toBeTruthy()
+})
