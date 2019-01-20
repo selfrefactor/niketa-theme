d6e774569dc4a2b526f68b025185b5030438e510
Dejan Toteff <deyan8284@gmail.com>
2019-01-20 22:03:45 +0200

    test: sort colors

--- src/ants/sortColors.js
+++ src/ants/sortColors.js
@@ -1,16 +1,19 @@
 import importedColors from '../../colors.json'
+import { writeJsonAnt } from './writeJson.js';
 
 export function sortColors(){
   const keys = Object.keys(importedColors)
-
+  const sortedColors = {}
   keys.forEach(key => {
     const all = Object.values(importedColors[key]).map(x => x.toLowerCase())
-    all.sort((a,b) => a > b ? 1 : -1)
-    
+    all.sort((a,b) => a < b ? 1 : -1)
+
     const newColor = {}
     all.forEach((sortedColor, i) => {
       newColor[i] = sortedColor
     })
-    console.log({newColor})
+    sortedColors[key] = newColor
   })
+
+  writeJsonAnt('colors.json', sortedColors)
 }
