bfe10d02c1b175eda21fb00098a5ee1edc2dace8
Dejan Toteff <deyan8284@gmail.com>
2019-04-11 21:14:23 +0300

    feat@theme circus people

--- src/base.spec.js
+++ src/base.spec.js
@@ -5,10 +5,11 @@ test('happy', () => {
 })
 
 test('random', () => {
-  [ 'foo', 'bar', 'baz','x','y','z' ].map(x => baseRandom(x))
+  [ 'foo', 'bar', 'baz', 'x', 'y', 'z' ].map(x => baseRandom(x))
 })
 
 test('random shade', () => {
   const result = 'foobar'.split('').map(randomShade)
   console.log({ result })
 })
+
