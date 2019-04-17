c6c0ec51d49c03e667ecc994d4c9f42b1a8e7d90
Dejan Toteff <deyan8284@gmail.com>
2019-04-18 00:20:10 +0300

    feat: sort color by brightness

--- src/ants/sortColors.js
+++ src/ants/sortColors.js
@@ -1,22 +1,30 @@
 import importedColors from '../../colors.json'
-import { writeJsonAnt } from './writeJson.js';
+import { writeJsonAnt } from './writeJson.js'
+import hexSorter from 'hexsorter'
 
 export function sortColors(){
   const keys = Object.keys(importedColors)
   const sortedColors = {}
   keys.forEach(key => {
-    const all = Object.values(importedColors[key]).map(x => x.toLowerCase())
-    all.sort((a,b) => a < b ? 1 : -1)
+    const all = Object.values(importedColors[ key ]).map(x => x.toLowerCase())
+    all.sort((a, b) => {
+      if (!a) return 1
+      if (!b) return -1
+
+      const brighter = hexSorter.mostBrightColor([ a, b ])
+
+      return brighter === a ? -1 : 1
+    })
 
     const newColor = {}
     all.forEach((sortedColor, i) => {
-      newColor[i] = sortedColor
+      newColor[ i ] = sortedColor
     })
-    sortedColors[key] = newColor
+    sortedColors[ key ] = newColor
   })
 
   writeJsonAnt(
-    'colors.json', 
+    'colors.json',
     sortedColors
   )
 
