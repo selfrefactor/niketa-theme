15a695a1254eaa39161d8c90a9404b292753a6d2
Dejan Toteff <deyan8284@gmail.com>
2019-04-20 20:45:31 +0300

    feat@progress change circus people | increase levels

--- src/bees/createTheme.js
+++ src/bees/createTheme.js
@@ -2,8 +2,9 @@ import { range } from 'rambdax'
 
 export function createThemeBee(rules, originTheme){
   const keys = Object.keys(rules)
-
-  return range(0, rules[ keys[ 0 ] ].length).map(i => {
+  const len = rules[ keys[ 0 ] ].length
+  
+  return range(0, len).map(i => {
     const newThemeColors = {}
     keys.forEach(path => {
       newThemeColors[ path ] = rules[ path ][ i ]
