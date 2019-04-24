ce51eef1882bfc7a8049e734cff800642505c209
Dejan Toteff <deyan8284@gmail.com>
2019-04-24 22:50:24 +0300

    feat@progress permutation

--- src/ants/mini/generateBase.spec.js
+++ src/ants/mini/generateBase.spec.js
@@ -1,11 +1,56 @@
 import { generateBase, generateBaseRandom } from './generateBase'
+import {
+  equals,
+  flatten,
+  range,
+  shuffle,
+  sort,
+  uniqBy,
+} from 'rambdax'
 
 test.skip('happy', () => {
   generateBase('boring')
 })
 
-test('random', () => {
-  
+test.skip('random', () => {
   expect(() => generateBaseRandom('sk')).not.toThrow()
+})
+
+function isUniq(x, holder){
+  const [found] = holder.filter(holderInstance => equals(holderInstance,x))
+  return !found
+}
+
+function pushUniq(x, holder){
+  if(!isUniq(x,holder)) return
+  holder.push(x)
+}
+
+const PERMUTATION_BASE = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+]
+
+function permutation(){
+  const holder = []
+  
+  range(0,100).forEach(_ => {
+    pushUniq(
+      shuffle(PERMUTATION_BASE),
+      holder
+    )
+  });
+
+  return holder
+}
+
+test('random', () => {
+  const x = permutation()
+  console.log({x, sk: x.length})
+  // expect(() => generateBaseRandom('sk')).not.toThrow()
 })
 
