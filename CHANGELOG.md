c8ad698078c8d5385007445aa5d79a856413cdd3
Dejan Toteff <deyan8284@gmail.com>
2019-05-31 16:47:12 +0300

    docs: changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# CHANGELOG
+
+## 7.0.0
+
+Major updates in several themes:
+
+- NiketaOwl
+
+- CircusBrother
+
+- Brave neighbour
+
+- Circus whiskey
+
+- Advanced engine
+
+> Last version before these updates is `6.24.11`
--- src/ants/populate_screens/populateScreens.js
+++ src/ants/populate_screens/populateScreens.js
@@ -1,42 +1,30 @@
 import { readdirSync } from 'fs'
 import { copySync } from 'fs-extra'
 import { resolve } from 'path'
-import { dotCase , snakeCase } from 'string-fn'
+import { dotCase, snakeCase } from 'string-fn'
 import { sort, pluck } from 'rambdax'
 import themes from '../../../exported.json'
- 
+
 export function populateScreensAnt(){
-  const sortFn = (a, b) => a > b ? 1 : -1
-  const base = resolve(
-    __dirname,
-    '../../../files'
-  )   
+  const sortFn = (a, b) => (a > b ? 1 : -1)
+  const base = resolve(__dirname, '../../../files')
   const lernaBase = resolve(
     __dirname,
     '../../../../niketa-themes/packages'
-  )   
+  )
   const screens = readdirSync(`${ base }/raw_screens`)
   const sortedScreens = sort(sortFn, screens)
-  const screensSources = sortedScreens.map(
-    x => `${ base }/raw_screens/${ x }`
-  )
- 
+  const screensSources = sortedScreens.map(x => `${ base }/raw_screens/${ x }`)
+
   const themesNames = sort(sortFn, pluck('label', themes))
   const screenDestinations = themesNames.map(
     x => `${ base }/${ dotCase(x) }.png`
   )
   const lernaDestinations = themesNames.map(
-    x => `${ lernaBase }/${snakeCase(x)}/theme/${ dotCase(x) }.png`
+    x => `${ lernaBase }/${ snakeCase(x) }/theme/${ dotCase(x) }.png`
   )
   screensSources.forEach((screenPath, i) => {
-    copySync(
-      screenPath,
-      screenDestinations[ i ]
-    )
-    copySync(
-      screenPath,
-      lernaDestinations[ i ]
-    )
+    copySync(screenPath, screenDestinations[ i ])
+    copySync(screenPath, lernaDestinations[ i ])
   })
-
 }
