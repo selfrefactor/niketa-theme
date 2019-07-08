ba936392b93111409cca93413ec30644a80d906b
Dejan Toteff <deyan8284@gmail.com>
2019-07-08 21:37:24 +0300

    feat@progress setup missing chrome colors | remove adorable and comical suggestions

--- src/createMultipleTheme.spec.js
+++ src/createMultipleTheme.spec.js
@@ -5,38 +5,67 @@ import { generateThemeDataBee } from './bees/generateThemeData'
 import { readJsonAnt } from './ants/readJson'
 import { maybe, map, defaultTo, replace, switcher } from 'rambdax'
 
-const newSuggestions = [
-  'actual',
-  'astral',
-  'adorable',
-  'calm',
-  'comical',
-]
-
 export const baseColors = {
-  'diffEditor.removedTextBackground'  : '#64B5F655',
-  'diffEditor.insertedTextBackground' : '#9c824a55',
-  'activityBar.background'            : '#C4BE9D',
-  'editor.selectionBackground'        : 'MAIN_COLOR55',
-  'editorBracketMatch.background'     : '#B1365Bf3',
-  'editorBracketMatch.border'         : '#9F7E6Bf3',
-  'editorGroupHeader.tabsBackground'  : 'MAIN_COLOR',
-  'editorLineNumber.foreground'       : '#2a3343a9',
-  'scrollbarSlider.background'        : 'MAIN_COLOR',
-  'scrollbarSlider.hoverBackground'   : '#C4BE9D',
-  'sideBar.background'                : 'MAIN_COLOR',
-  'statusBar.background'              : 'MAIN_COLOR',
-  'editor.lineHighlightBackground'    : 'MAIN_COLOR25',
-  'tab.activeBackground'              : 'BACK_COLOR',
-  'tab.activeForeground'              : '#35495f',
-  'tab.inactiveForeground'            : '#fff',
-  'tab.inactiveBackground'            : 'MAIN_COLOR',
-  'editor.background'                 : '#FAF8F3',
+  "activityBar.background": "#C4BE9D",
+  "badge.background": "#aaa",
+  "badge.foreground": "#faa",
+  "diffEditor.insertedTextBackground": "#9c824a55",
+  "diffEditor.removedTextBackground": "#64B5F655",
+  "editor.background": "#FAF8F3",
+  "editor.findMatchBackground":"#95a5a677",
+  "editor.findMatchHighlightBackground":"#71aac355",
+  "editor.findRangeHighlightBackground":"#3f706366",
+  "editor.lineHighlightBackground": "MAIN_COLOR25",
+  "editor.lineHighlightBorderx":"#9a9b9411",
+  "editor.selectionBackground": "MAIN_COLOR55",
+  "editor.selectionHighlightBackground":"MAIN_COLOR88",
+  "editor.wordHighlightBackground":"MAIN_COLORaa",
+  "editor.wordHighlightStrongBackground":"MAIN_COLORdd",
+  "editorBracketMatch.background": "#B1365Bf3",
+  "editorBracketMatch.border": "#9F7E6Bf3",
+  "editorCursor.foreground": "#544",
+  "editorGroupHeader.tabsBackground": "MAIN_COLOR",
+  "editorLineNumber.foreground": "#2a3343a9",
+  "editorLink.activeForeground":"#034694",
+  "errorForeground": "#B1365Bf3",
+  "focusBorder": "#525e54",
+  "foreground": "#B06775",
+  "list.activeSelectionBackground": "MAIN_COLOR",
+  // TODO: use bright according to mode
+  "list.activeSelectionForeground": "#fff",
+  "list.dropBackground": "#455a64",
+  "list.focusBackground": "#87a192",
+  "list.highlightForeground": "#fff",
+  "list.hoverBackground": "#35495f",
+  "list.hoverForeground": "BACK_COLOR",
+  "list.inactiveSelectionBackground": "#fff",
+  "list.inactiveSelectionForeground": "#30322e",
+  "scrollbarSlider.background": "MAIN_COLOR",
+  "scrollbarSlider.hoverBackground": "#C4BE9D",
+  "selection.background": "#ebe6d9",
+  "sideBar.background": "MAIN_COLOR",
+  "sideBar.border": "#445250c1",
+  "sideBar.foreground": "#f9f4f4",
+  "sideBarSectionHeader.background": "#aebabee9",
+  "sideBarSectionHeader.foreground": "#2a3343e9",
+  "sideBarTitle.foreground": "#30322ed1",
+  "statusBar.background": "MAIN_COLOR",
+  "statusBar.foreground": "#35495f",
+  "tab.activeBackground": "BACK_COLOR",
+  "tab.activeBorder": "#35495f",
+  "tab.activeForeground": "#35495f",
+  "tab.border": "MAIN_COLOR",
+  "tab.inactiveBackground": "MAIN_COLOR",
+  "tab.inactiveForeground": "#fff",
+  "tab.unfocusedActiveBackground": "MAIN_COLOR",
+  "tab.unfocusedActiveBorder": "MAIN_COLOR",
+  "tab.unfocusedActiveForeground": "#fff",
+  "widget.shadow": "#8382aebb"
 }
 
 function getBaseColors(mode, actualBack){
   const chromeMainColor = switcher(mode)
-    .is('advanced', '#bdc3c7')
+    .is('advanced', 'MAIN_COLOR')
     .is('brave', '#bbc0c4')
     .is('circus', '#b7bcbf')
     .default('#b0b4b4')
