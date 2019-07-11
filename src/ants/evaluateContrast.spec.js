50e2ff672e91b300eb74e70ccf775fe6fb82df60
Dejan Toteff <deyan8284@gmail.com>
2019-07-11 08:47:17 +0300

    feat@small

--- src/ants/evaluateContrast.spec.js
+++ src/ants/evaluateContrast.spec.js
@@ -1,15 +1,4 @@
-const getContrastRatio = require('get-contrast-ratio')
-import { readJsonSync, outputJsonSync } from 'fs-extra'
 import { evaluateContrast } from './evaluateContrast'
-export function evaluateContrastx(themeColors){
-  // const a = getContrastRatio.default('#433', '#fafafa');
-  const current = readJsonSync(path)
-  const newState = {
-    ...current,
-    'workbench.colorTheme' : 'BraveHomer',
-  }
-  outputJsonSync(path, newState, { spaces : 2 })
-}
 
 test('happy', () => {
   evaluateContrast()
