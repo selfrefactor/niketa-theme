20831c2268a0d82ca94a63ed38dbb93bf7927bf9
Dejan Toteff <deyan8284@gmail.com>
2019-07-28 11:24:42 +0300

    feat@progress trending theme

--- src/ants/trending_colors/permutation.spec.js
+++ src/ants/trending_colors/permutation.spec.js
@@ -5,7 +5,6 @@ test('happy', () => {
   const list = []
   console.log(
     getCombinations(range(0, 10), 3, list)
-    // getCombinations(range(0,30), 3, list)
   )
   console.log(list)
 
--- src/ants/trending_colors/trendingColors.js
+++ src/ants/trending_colors/trendingColors.js
@@ -1,22 +1,39 @@
 import { readJsonAnt } from '../../ants/readJson'
 import { writeJsonAnt } from '../../ants/writeJson'
-import {getCombinations} from './permutation'
-import { glue, piped, sort, range, tap, take, pluck, prepend , map } from 'rambdax'
+const getContrastRatio = require('get-contrast-ratio')
+import { getCombinations } from './permutation'
+import { glue, piped, sort, range, tap, take, pluck, prepend, map, mapAsync, flatten , any , filter , toDecimal } from 'rambdax'
 import axios from 'axios'
 
 const SAVED = 'src/ants/trending_colors/saved.json'
 const SAVED_COLORS = 'src/ants/trending_colors/colors.json'
+const SAVED_SK = 'src/ants/trending_colors/sk.json'
 
-async function getColors(){
+function getContrast(a,b){
+  return getContrastRatio.default(a, b)
+}
+
+async function getAllColors(offset){
   const url = glue(`
     http://www.colourlovers.com/api/colors?format=json
     numResults=100
     orderCol=dateCreated
     sortBy=DESC
+    resultOffset=${ offset }
     `, '&')
   const { data } = await axios.get(url)
+
+  return data
+}
+
+async function getColors(){
+  const colorsRaw = await mapAsync(
+    async i => getAllColors(i * 100),
+  )(range(0, 10))
+  const colors = flatten(colorsRaw)
+
   const sk = piped(
-    data,
+    colors,
     sort((a, b) => {
       if (a.numVotes !== b.numVotes){
         return a.numVotes > b.numVotes ? -1 : 1
@@ -26,35 +43,72 @@ async function getColors(){
       return a.numViews > b.numViews ? -1 : 1
     }),
     // tap(x => x.map(xx => console.log(xx.numVotes, xx.numViews))),
-    take(20),
+    // take(20),
   )
-  console.log(sk[ 0 ], 'SAVED')
+  console.log(sk[ 0 ], sk.length, 'SAVED')
   writeJsonAnt(SAVED, sk)
 }
 
 const restoreColors = () => readJsonAnt(SAVED)
 
-function getIndexes(){
+function getIndexes(limit){
   const indexList = []
-  getCombinations(range(0,10), 3, indexList)
+  getCombinations(range(0, limit), 3, indexList)
+
   return indexList.map(x => x.split(' ').map(Number))
 }
 
-function evaluateCombination(indexListInstance, colors){
-  
+function evaluateCombination(indexListInstance, colors, background){
+  const color1 = colors[indexListInstance[0]]
+  const color2 = colors[indexListInstance[1]]
+  const color3 = colors[indexListInstance[2]]
+  const betweenContrast = [
+    getContrast(color1, color2),
+    getContrast(color1, color3),
+    getContrast(color2, color3),
+  ]
+  const okBetween = any(x => x < 1.5,betweenContrast)
+  if(okBetween) return false
+  const compareToBackground = [
+    getContrast(color1, background),
+    getContrast(color2, background),
+    getContrast(color3, background),
+  ]
+  if(any(x => x < 1.5,compareToBackground)) return false
+  const sorted = [color1, color2, color3].sort()
+  const maxBetween = Math.max(...betweenContrast)
+  const maxBackground = Math.max(...compareToBackground)
+
+  return {colors: sorted, maxBetween, maxBackground}
 }
 
+const BACKGROUND = '#eaeaf4'
+const LIMIT = 20
 export async function trendingColorsAnt(reload = true){
   const trendingColors = reload ? restoreColors() : await getColors()
   const colors = piped(
     trendingColors,
+    take(LIMIT),
     pluck('hex'),
     map(prepend('#'))
   )
-  const indexList = getIndexes()  
-  console.log({indexList})
-  const aaa = evaluateCombination(indexList[0], colors)  
+  const indexList = getIndexes(LIMIT)
+  // console.log({indexList})
+  const sk = piped(
+    indexList,
+    map(indexListInstance => evaluateCombination(indexListInstance, colors,BACKGROUND)),
+    filter(Boolean),
+    sort((a, b) => {
+      if (Math.floor(a.maxBackground) === Math.floor(b.maxBackground)){
+        return a.maxBetween > b.maxBetween ? -1 : 1
+      }
 
+      return a.maxBackground > b.maxBackground ? -1 : 1
+    }),
+  )
+    
+  // writeJsonAnt(SAVED_SK, sk)
+  console.log(toDecimal)
+  
 }
 
-// writeJsonAnt(SAVED_COLORS, plucked.map(prepend('#')))
