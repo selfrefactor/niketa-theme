8a954c4272fb5a799aff3763a1f266817749e28a
Dejan Toteff <deyan8284@gmail.com>
2019-07-28 16:12:50 +0300

    feat: forbid colors and use predicate with trending

--- src/ants/trending_colors/trendingColors.js
+++ src/ants/trending_colors/trendingColors.js
@@ -2,11 +2,12 @@ import { readJsonAnt } from '../../ants/readJson'
 import { writeJsonAnt } from '../../ants/writeJson'
 const getContrastRatio = require('get-contrast-ratio')
 import { getCombinations } from './permutation'
-import { glue, piped, sort, range, tap, take, pluck, prepend, map, mapAsync, flatten, any, filter, toDecimal, maybe, complement, splitEvery, nth, omit } from 'rambdax'
+import { glue, piped, sort, range, tap, take, pluck, prepend, map, mapAsync, flatten, any, filter, toDecimal, maybe, complement, splitEvery, nth, omit, shuffle, uniq } from 'rambdax'
 import axios from 'axios'
 
 const SAVED = 'src/ants/trending_colors/saved.json'
 const SAVED_SK = 'src/ants/trending_colors/sk.json'
+const SAVED_FILTERED = 'src/ants/trending_colors/filtered.json'
 
 function getContrast(a, b){
   return getContrastRatio.default(a, b)
@@ -14,7 +15,9 @@ function getContrast(a, b){
 const BLUE_BASE = '#00f'
 const RED_BASE = '#f00'
 const DARK_BASE = '#000'
+const LIGHT_BASE = '#fff'
 const isRed = x => getContrast(x, RED_BASE) < 1.15
+export const isTooLight = x => getContrast(x, LIGHT_BASE) < 2.2
 const isDark = x => {
   const aa = getContrast(x, DARK_BASE) < 1.5
   // console.log(getContrast(x, BLUE_BASE),getContrast(x, RED_BASE),  getContrast(x, DARK_BASE), x,9,aa);
@@ -35,15 +38,8 @@ function filterAgainstTwoBlues(colors){
   return blues.length === 1 || blues.length === 0
 }
 
-const FOO = {
-  COLORS : {
-    COLOR_0 : '#2C226F',
-    COLOR_1 : '#646EA7',
-    COLOR_2 : '#8DC0D4',
-  },
-}
-// console.log(filterAgainstTwoBlues([FOO.COLORS.COLOR_0, FOO.COLORS.COLOR_1,FOO.COLORS.COLOR_2]))
 async function getColorsFragment(offset){
+  console.log({ offset })
   const url = glue(`
     http://www.colourlovers.com/api/colors?format=json
     numResults=100
@@ -119,22 +115,34 @@ function evaluateCombination(indexListInstance, colors, background){
   }
 }
 
-function getLocalColors(limit){
+function getLocalColors(colorLovers, limit){
   const list = []
 
-  map((x, key) => map(xx => list.push(xx), x))(readJsonAnt('colors.json'))
+  map(x => map(xx => list.push(xx), x))(readJsonAnt('colors.json'))
 
-  return piped(
+  const localColors = piped(
     list,
     filter(x => x.length === 7)
   )
+  if (!colorLovers) return localColors
+  const halfLimit = Math.floor(limit / 2)
+
+  return [
+    ...take(halfLimit, colorLovers),
+    ...take(halfLimit, shuffle(localColors)),
+  ]
 }
 
 const BACKGROUND = '#FAF8F3'
 const LIMIT = 130
 const INDEX = 0
 
-export async function trendingColorsAnt({ reload, useLocalColors }){
+export async function trendingColorsAnt({ reload, useLocalColors, mixFlag, predicate }){
+  if (predicate){
+    const filtered = predicate(readJsonAnt(SAVED_SK))
+
+    return writeJsonAnt(SAVED_FILTERED, filtered)
+  }
   const trendingColors = reload ? restoreColors() : await getColors(100)
   const getColorLovers = () => piped(
     trendingColors,
@@ -143,18 +151,23 @@ export async function trendingColorsAnt({ reload, useLocalColors }){
     splitEvery(LIMIT),
     nth(INDEX),
   )
-  const colors = useLocalColors ?
-    getLocalColors(LIMIT) :
-    getColorLovers()
- 
-  const indexList = getIndexes(useLocalColors ? take(LIMIT, colors.length).length : LIMIT)
+  const colors = maybe(
+    mixFlag,
+    getLocalColors(getColorLovers(), LIMIT),
+    useLocalColors ?
+      getLocalColors(LIMIT) :
+      getColorLovers()
+  )
+  const numberIterations = useLocalColors || mixFlag ? take(LIMIT, colors.length).length : LIMIT
+  const indexList = getIndexes(numberIterations)
+  console.log(colors.length, indexList.length)
   const sk = piped(
     indexList,
     map(indexListInstance => evaluateCombination(indexListInstance, colors, BACKGROUND)),
     filter(Boolean),
     filter(x => filterAgainstTwoBlues(x.colors)),
-    filter(x => x.minBetween > 2),
-    filter(x => x.minBackground > 2.04),
+    // filter(x => x.minBetween > 2),
+    // filter(x => x.minBackground > 2.04),
     sort((a, b) => {
       if (a.minBetween === b.minBetween){
         return a.maxBetween > b.maxBetween ? -1 : 1
--- src/ants/trending_colors/trendingColors.spec.js
+++ src/ants/trending_colors/trendingColors.spec.js
@@ -1,10 +1,22 @@
-import { trendingColorsAnt } from './trendingColors'
+import { filter, complement } from 'rambdax'
+import { trendingColorsAnt, isTooLight } from './trendingColors'
 
 test('happy', async () => {
   jest.setTimeout(10 * 60 * 1000)
+  const FORBIDDEN = ['#6561F5']
+  const predicate = x => {
+    const passForbidden = filter(a => FORBIDDEN.includes(a), x.COLORS)
+    if(Object.keys(passForbidden).length > 0) return false
+
+    const numLights = filter(isTooLight, x.COLORS)
+        
+    return Object.keys(numLights).length===0
+  }
   await trendingColorsAnt({
     useLocalColors : false,
     reload         : true,
+    mixFlag        : true,
+    predicate: filter(predicate)
   })
   // expect().toBe()
 })
