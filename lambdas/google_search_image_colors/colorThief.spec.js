4d91af8f84c34cf80df63eebe6862f8e6a8a962a
Dejan Toteff <deyan8284@gmail.com>
2019-07-30 18:42:07 +0300

    chore@important remove color thief

--- lambdas/google_search_image_colors/colorThief.spec.js
+++
@@ -1,9 +0,0 @@
-const {getColor} = require('./colorThief')
-const {resolve} = require('path')
-
-test('happy', async () => {
-    const result = await getColor(resolve(__dirname, '../../files/niketa_young.jpg'))
-    console.log(result);
-    
-    // expect(result).toBe(expected)
-})
