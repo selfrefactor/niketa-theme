6806b279272893bcba329fa93533328dbd3d76b3
Dejan Toteff <deyan8284@gmail.com>
2019-07-30 22:03:42 +0300

    feat: apply trending with only one triangle

--- src/applyTrending.spec.js
+++ src/applyTrending.spec.js
@@ -53,7 +53,6 @@ export const baseColors = {
   'editorLink.activeForeground'               : '#034694',
   'errorForeground'                           : '#B1365Bf3',
   'focusBorder'                               : '#525e54',
-  // 'foreground'                           : '#B06775',
   'scrollbarSlider.background'                : 'MAIN_COLOR',
   'scrollbarSlider.hoverBackground'           : '#C4BE9D',
   'selection.background'                      : '#ebe6d9',
@@ -101,20 +100,19 @@ function getBaseColors(mode, actualBack){
   )(withMainColor)
 }
 
+
+
 export const SETTINGS = {}
 SETTINGS[ 0 ] = {
   mode    : 'advanced',
   label   : 'bat',
   COLOR_0 : '#801336',
-  COLOR_1 : '#93b5b3',
-  COLOR_2 : '#018574',
+COLOR_1 : '#93b5b3',
+COLOR_2 : '#018574',
 }
 SETTINGS[ 1 ] = {
   mode    : 'advanced',
   label   : 'cat',
-  COLOR_0 : '#6561F5',
-  COLOR_1 : '#D77DFF',
-  COLOR_2 : '#5D0450',
 }
 SETTINGS[ 2 ] = {
   mode  : 'advanced',
@@ -129,116 +127,30 @@ SETTINGS[ 3 ] = {
 SETTINGS[ 4 ] = {
   mode    : 'advanced',
   label   : 'hook',
-  COLOR_0 : '#556AE7',
-  COLOR_1 : '#50C5C2',
-  COLOR_2 : '#132333',
 }
 // dancing days
 SETTINGS[ 5 ] = {
   mode    : 'advanced',
   label   : 'immigrant',
-  COLOR_0 : '#50C5C2',
-  COLOR_1 : '#132333',
-  COLOR_2 : '#847545',
 }
 
-SETTINGS[ 6 ] = {
-  mode    : 'advanced',
-  label   : 'mystery',
-  COLOR_0 : '#E8045A',
-  COLOR_1 : '#d4b106',
-  COLOR_2 : '#282c34',
-}
+const permutations = [
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,2,1],
+  [3,1,2],
+]
+permutations.forEach((indexes, i) => {
+  SETTINGS[i+1] = {
+    ...SETTINGS[i+1],
+    COLOR_0: SETTINGS[0][`COLOR_${indexes[0] -1}`],
+    COLOR_1: SETTINGS[0][`COLOR_${indexes[1] -1}`],
+    COLOR_2: SETTINGS[0][`COLOR_${indexes[2] -1}`],
+  }
+})
+console.log(SETTINGS);
 
-SETTINGS[ 7 ] = {
-  mode    : 'brave',
-  label   : 'habits',
-  COLOR_2 : '#25164C',
-  COLOR_0 : '#847545',
-  COLOR_1 : '#f38b80',
-}
-
-SETTINGS[ 8 ] = {
-  mode    : 'brave',
-  label   : 'homer',
-  COLOR_0 : '#847545',
-  COLOR_1 : '#f38b80',
-  COLOR_2 : '#820014',
-}
-SETTINGS[ 9 ] = {
-  mode    : 'brave',
-  label   : 'love',
-  COLOR_0 : '#7A5DF1',
-  COLOR_1 : '#d4a12a',
-  COLOR_2 : '#85144b',
-}
-// lemon song
-SETTINGS[ 10 ] = {
-  mode    : 'brave',
-  label   : 'neighbour',
-  COLOR_0 : '#0F5037',
-  COLOR_1 : '#7A5DF1',
-  COLOR_2 : '#d4a12a',
-}
-SETTINGS[ 11 ] = {
-  mode    : 'circus',
-  label   : 'ajax',
-  COLOR_2 : '#8B1518',
-  COLOR_1 : '#847545',
-  COLOR_0 : '#f38b80',
-}
-// stop
-// label      : 'since.loving',
-SETTINGS[ 12 ] = {
-  back    : '#f9f6f1',
-  mode    : 'circus',
-  label   : 'brother',
-  COLOR_2 : '#25164C',
-  COLOR_1 : '#da3b01',
-  COLOR_0 : '#60b49b',
-}
-// label      : 'tea.for'
-SETTINGS[ 13 ] = {
-  back    : '#f9f6f1',
-  mode    : 'circus',
-  label   : 'people',
-  COLOR_0 : '#9EADD0',
-  COLOR_1 : '#1B7F9D',
-  COLOR_2 : '#702861',
-}
-// label      : 'in.light',
-SETTINGS[ 14 ] = {
-  mode    : 'circus',
-  label   : 'whisky',
-  COLOR_0 : '#0B0A6B',
-  COLOR_1 : '#0DBDF1',
-  COLOR_2 : '#028B02',
-}
-
-SETTINGS[ 15 ] = {
-  mode    : 'niketa',
-  label   : 'owl',
-  COLOR_0 : '#1B7F9D',
-  COLOR_1 : '#C89ECB',
-  COLOR_2 : '#0B0A6B',
-}
-
-SETTINGS[ 16 ] = {
-  mode    : 'niketa',
-  label   : 'bear',
-  COLOR_0 : '#C66534',
-  COLOR_1 : '#3c6e5b',
-  COLOR_2 : '#532053',
-}
-SETTINGS[ 17 ] = {
-  mode    : 'niketa',
-  label   : 'moon',
-  back    : '#c1bcae',
-  COLOR_0 : '#696040',
-  COLOR_1 : '#295879',
-  COLOR_2 : '#7f0c29',
-}
-// WINNERS: brother ajax
 // const SOLE_BACKGROUND = '#FAF8F3'
 const SOLE_BACKGROUND = '#f3f0e0'
 
