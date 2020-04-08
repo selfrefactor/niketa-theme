5c5c0f9da9cdf4665df44bb0cf14d07db798c358
Dejan Toteff <deyan8284@gmail.com>
2020-04-08 23:28:16 +0300

    feat@stop export to mono

--- lambdas/export_to_mono/exportToMono.js
+++ lambdas/export_to_mono/exportToMono.js
@@ -3,63 +3,102 @@ import { copy, outputFile, outputJson, readJson } from 'fs-extra'
 import { exec } from 'helpers-fn'
 import { load, save } from 'package-storage'
 import { resolve as resolveMethod } from 'path'
-import { replace, sort, piped } from 'rambdax'
-import { dotCase, snakeCase, titleCase } from 'string-fn'
-
-import { readJsonAnt, resolve } from '../../src/ants/readJson'
-const sortFn = (a, b) => a > b ? -1 : 1
+import { piped, replace } from 'rambdax'
+import { dotCase, pascalCase, snakeCase } from 'string-fn'
 
 const STANDALONES_VERSION_KEY = 'standaloneVersion'
 
-async function bumpVersion(){
+async function bumpVersion(version){
+  const [ major, minor, patch ] = version.split('.').map(Number)
+  const newVersion = `${ major }.${ minor }.${ patch + 1 }`
 
+  return save(
+    STANDALONES_VERSION_KEY, newVersion, undefined, true
+  )
 }
 
 async function getVersion(){
-  return load(STANDALONES_VERSION_KEY, undefined,true)
-} 
+  return load(
+    STANDALONES_VERSION_KEY, undefined, true
+  )
+}
 
 const PROJECT_ROOT = resolveMethod(__dirname, '../../')
 
-function createReadme({themeName, asDot, asSnake}){
-  const templateData = readFileSync(`${__dirname}/template/README.md`).toString()
+function createReadme({ themeName, asDot, asSnake }){
+  const templateData = readFileSync(`${ __dirname }/template/README.md`).toString()
 
   return piped(
     templateData,
     replace(/FooBar/g, themeName),
     replace(/foo\.bar/g, asDot),
-    replace(/foo_bar/g, asSnake),
+    replace(/foo_bar/g, asSnake)
   )
 }
 
-async function getPackageJson({themeName, asDot, asSnake}){
-  const content = await readJson(`${__dirname}/template/package.json`)
-  console.log(content)  
+async function getPackageJson({ version, themeName, asDot }){
+  const content = await readJson(`${ __dirname }/template/package.json`)
+  const contributes = {
+    themes : [
+      {
+        label   : themeName,
+        uiTheme : 'vs',
+        path    : `./theme/${ themeName }.json`,
+      },
+    ],
+  }
+
+  return {
+    ...content,
+    name        : themeName,
+    displayName : themeName,
+    version,
+    icon        : replace(
+      'foo.bar', asDot, content.icon
+    ),
+    contributes,
+  }
 }
 
-export async function exportToMono(themeName){
+export async function exportToMono(themeNameRaw){
+  const themeName = pascalCase(themeNameRaw)
   const version = await getVersion()
   const asDot = dotCase(themeName)
   const asSnake = snakeCase(themeName)
-  const readmeContent =  createReadme({themeName, asDot, asSnake})
-  const packageJsonContent = await getPackageJson({version, themeName, asDot, asSnake})
+  const readmeContent = createReadme({
+    themeName,
+    asDot,
+    asSnake,
+  })
+  const packageJsonContent = await getPackageJson({
+    version,
+    themeName,
+    asDot,
+  })
   const filePathBase = resolveMethod(__dirname,
     '../../../niketa-themes/packages')
-  
-    const DESTINATION_ROOT = `${ filePathBase }/${ asSnake }`
-    
-    const themeSource = `${PROJECT_ROOT}/themes/${ themeName }.json`
-    const themeDestination = `${ DESTINATION_ROOT }/theme/${ themeName }.json`
-    
-    const screenSource = `${PROJECT_ROOT}/files/${ asDot }.png`
-    const screenDestination = `${DESTINATION_ROOT}/theme/${ asDot }.png`
-    const readmeDestination = `${DESTINATION_ROOT}/README.md`
 
-    await copy(screenSource, screenDestination, {overwrite: true})
-    await copy(themeSource, themeDestination, {overwrite: true})
-    await outputFile(readmeDestination, readmeContent)
- 
-    return console.log({themeDestination})  
+  const DESTINATION_ROOT = `${ filePathBase }/${ asSnake }`
+
+  const themeSource = `${ PROJECT_ROOT }/themes/${ themeName }.json`
+  const screenSource = `${ PROJECT_ROOT }/files/${ asDot }.png`
+
+  const themeDestination = `${ DESTINATION_ROOT }/theme/${ themeName }.json`
+  const screenDestination = `${ DESTINATION_ROOT }/theme/${ asDot }.png`
+  const readmeDestination = `${ DESTINATION_ROOT }/README.md`
+  const packageJsonDestination = `${ DESTINATION_ROOT }/package.json`
+
+  await copy(
+    screenSource, screenDestination, { overwrite : true }
+  )
+  await copy(
+    themeSource, themeDestination, { overwrite : true }
+  )
+  await outputFile(readmeDestination, readmeContent)
+  await outputJson(
+    packageJsonDestination, packageJsonContent, { spaces : 2 }
+  )
+  // await bumpVersion(version)
 
   // await exec({
   //   command : 'run d feat@bump patch',
@@ -74,4 +113,3 @@ export async function exportToMono(themeName){
   //   cwd     : destination,
   // })
 }
-
--- lambdas/export_to_mono/exportToMono.spec.js
+++ lambdas/export_to_mono/exportToMono.spec.js
@@ -1,8 +1,25 @@
 import { ms } from 'string-fn'
+import { mapAsync } from 'rambdax'
 import { exportToMono } from './exportToMono'
 
 jest.setTimeout(ms('5 minutes'))
 
+const themes = [
+  'LedZeppelin',
+  'SweatLeaf',
+  'HelloSpaceboy',
+  'CommunicationBreakdown',
+  'GlassOnion',
+  'DancingDays',
+  'FunkyDrummer',
+  'KozmicBlues',
+  'StrangeBrew',
+]
+
+async function iterable(themeName){
+  await exportToMono(themeName)
+}
+
 test('happy', async () => {
-  await exportToMono('StrangeBrew')
+  // await mapAsync(iterable, themes)
 })
