6cf9d56163e2e3111f06e2f46feeac32065ba2e0
Dejan Toteff <deyan8284@gmail.com>
2020-04-28 14:04:40 +0300

    feat@important allow spin of colors

---
+++ src/assets/themes-colors.js
@@ -0,0 +1,27 @@
+exports.CommunicationBreakdown = [
+  '#38978D',
+  '#b66ae4',
+  '#B1365B',
+  '#0068a8',
+  '#480032',
+]
+exports.DancingDays = [ '#b13695', '#38978D', '#614ad3', '#083358', '#df5831' ]
+exports.FunkyDrummer = [
+  '#b66ae4',
+  '#480032',
+  '#0068a8',
+  '#38978D',
+  '#B1365B',
+]
+exports.GlassOnion = [ '#bb9132', '#20366b', '#2c91af', '#861D4F', '#E9630D' ]
+exports.HelloSpaceboy = [
+  '#01676b',
+  '#6833b9',
+  '#b1336b',
+  '#192112',
+  '#0031df',
+]
+exports.KozmicBlues = [ '#a83c56', '#28305d', '#0068a8', '#d239a0', '#b66ae4' ]
+exports.LedZeppelin = [ '#0d8a81', '#00a8cc', '#a12b6d', '#d44578', '#3a0088' ]
+exports.StrangeBrew = [ '#00a8cc', '#222d60', '#953b69', '#b24826', '#01676b' ]
+exports.SweatLeaf = [ '#2f586f', '#a83c56', '#0031df', '#d239a0', '#bb9132' ]
