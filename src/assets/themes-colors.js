8be2b178577ff818867d068e0548b5dda8502a1a
Dejan Toteff <deyan8284@gmail.com>
2020-10-17 15:45:38 +0300

    chore@prepublish

--- src/assets/themes-colors.js
+++ src/assets/themes-colors.js
@@ -5,21 +5,28 @@ exports.CommunicationBreakdown = [
   '#089fbf',
   '#B1365B',
   '#480032',
-  '#089fbf',
+  '#F25F25',
   PURPLE,
 ]
-exports.DancingDays = [ PURPLE, '#1d74ed', '#0a0a0a', '#0b7c8f', '#e01114' ]
+exports.DancingDays = [
+  PURPLE,
+  '#1d74ed',
+  '#0a0a0a',
+  // '#0b7c8f',
+  '#008f00',
+  '#e01114',
+]
 
 exports.FunkyDrummer = [
-  '#38978D',
+  '#00C14E',
   '#480032',
   '#0068a8',
-  '#480032',
+  '#e01114',
   DARK_YELLOW,
 ]
 
 exports.GlassOnion = [
-  '#E9630D',
+  '#F25F25',
   '#20366b',
   '#861D4F',
   '#2c91af',
@@ -29,21 +36,36 @@ exports.GlassOnion = [
 exports.HelloSpaceboy = [
   '#df5831',
   '#38978D',
-  '#480032',
+  '#692340',
   '#0031df',
   '#b13695',
 ]
 
-exports.KozmicBlues = [ '#0068a8', '#480032', '#B1365B', '#38978D', PURPLE ]
+exports.KozmicBlues = [
+  '#0068a8',
+  '#480032',
+  '#d70a53',
+  '#953b69',
+  // '#00C14E',
+  PURPLE,
+]
 
-exports.LedZeppelin = [ '#3a0088', '#00a8cc', '#a12b6d', '#0d8a81', '#3a0088' ]
+exports.LedZeppelin = [ '#3a0088', '#00a8cc', '#a12b6d', '#0d8a81', '#D58FDB' ]
 
 exports.StrangeBrew = [
   '#222d60',
   '#01676b',
-  '#953b69',
+  // '#953b69',
+  '#fb607f',
   '#00a8cc',
   DARK_YELLOW,
 ]
 
-exports.SweatLeaf = [ '#0031df', DARK_YELLOW, '#2f586f', '#d239a0', '#a83c56' ]
+exports.SweatLeaf = [
+  '#0031df',
+  DARK_YELLOW,
+  '#2f586f',
+  '#FF3EBF',
+  // '#d239a0',
+  '#a83c56',
+]
