e920ce0eaf36046c873e0b5d5326f2147110e24f
Deyan Totev <deyan.totev.extern@seven.one>
2022-11-15 00:07:51 +0200

    chore@prepublish

--- src/assets/themes-colors.js
+++ src/assets/themes-colors.js
@@ -1,28 +1,27 @@
-const PURPLE = '#a963db'
+const PURPLE = '#551bb3'
 const DARK_YELLOW = '#C27D40'
-const BLUE = `#12a8c2`
-// exports.PUNCTUATION_COLOR = '#f0f'
-exports.PUNCTUATION_COLOR = '#00f'
-// first two colors are used in punctuation so they should be strong colors
+const BLUE = `#276bb4`
+exports.PUNCTUATION_COLOR = '#00ffab'
+
 exports.CommunicationBreakdown = [
   PURPLE,
   '#480032',
   '#F25F25',
-  '#B1365B',
+  '#d60257',
   '#089fbf',
 ]
 
-exports.DancingDays = ['#FF7B00', '#881dc7', '#7A7A7A', `#22a294`, `#ff0095`]
-exports.FunkyDrummer = [`#6E8DD5`, '#3EA57B', '#0a0a0a', DARK_YELLOW, '#CA7081']
-exports.GlassOnion = ['#cf7b60', BLUE, '#0a0a0a', '#953b69', '#f50141']
-exports.HelloSpaceboy = ['#df5831', '#38978D', '#692340', BLUE, '#b13695']
-exports.KozmicBlues = ['#218d54', PURPLE, '#3dcbc9', '#d70a53', '#000000']
-exports.LedZeppelin = ['#3a0088', '#FF3EBF', '#bb9132', '#3eb2b1', '#7A3E9D']
+exports.DancingDays = ['#FF7B00', '#881dc7', '#7A7A7A', `#4D9375`, `#cd526c`]
+exports.FunkyDrummer = [`#404467`, '#2a814d', '#0a0a0a', DARK_YELLOW, '#CA7081']
+exports.GlassOnion = ['#f78058', BLUE, '#360745', '#953b69', '#ae0c3e']
+exports.HelloSpaceboy = ['#d53d0c', '#38978D', '#3b0032', BLUE, '#d12771']
+exports.KozmicBlues = ['#218d54', PURPLE, '#3dcbc9', '#7d144c', '#000000']
+exports.LedZeppelin = ['#3a0088', '#d239a0', '#bb9132', '#3eb2b1', '#480656']
 exports.StrangeBrew = [
   '#222d60',
   '#01676b',
   '#fb607f',
-  '#3182CE',
+  '#1b66ff',
   DARK_YELLOW,
 ]
-exports.SweatLeaf = [BLUE, '#2f586f',DARK_YELLOW, '#FF3EBF', '#a83c56']
+exports.SweatLeaf = [BLUE, '#2f586f',DARK_YELLOW, '#F92672', '#85005b']
