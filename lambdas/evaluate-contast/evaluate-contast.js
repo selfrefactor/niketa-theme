74a960f3e2196c7e7e93af24e77e1995ec951102
Deyan Totev <deyan.totev.extern@seven.one>
2022-11-18 19:50:22 +0200

    chore@prepublish

--- lambdas/evaluate-contast/evaluate-contast.js
+++ lambdas/evaluate-contast/evaluate-contast.js
@@ -4,11 +4,24 @@ import { BACK_COLOR } from '../../src/assets/chrome-colors'
 import { allThemes } from '../../src/assets/themes-colors'
 
 export function evaluateContrast(){
-  let result = map((currentTheme) => {
+  let max = Infinity
+  let maxInfo
+  let minInfo
+  let min = 0
+  let result = map((currentTheme, name) => {
     return map(color => {
       const score = colorContrastRatioCalculator(color, BACK_COLOR)
+      if(score > min){
+        min = score
+        minInfo = `${ color } - ${ name } - ${score}`
+      } 
+      if(score < max){
+        max = score
+        maxInfo = `${ color } - ${ name } - ${score}`
+      }
       return `${ color } - ${ score }`
     }, currentTheme)
   }, allThemes)
-  console.log(result, `result`)
+  console.log(result)
+  console.log({maxInfo, minInfo})
 }
