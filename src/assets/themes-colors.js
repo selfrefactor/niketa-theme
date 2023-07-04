0c84718f925e54a72b5c2317ddfe5cd244a95e69
Deyan Totev <deyan.totev.extern@seven.one>
2023-07-04 12:03:47 +0300

    chore@prepublish

--- src/assets/themes-colors.js
+++ src/assets/themes-colors.js
@@ -1,6 +1,8 @@
 const PURPLE = '#774499'
-const DARK_YELLOW = '#C27D40'
-const BLUE = '#276bb4'
+const DARK_YELLOW = '#ff6369'
+// const DARK_YELLOW = '#C27D40'
+const BLUE = '#1b66ff'
+// const BLUE = '#276bb4'
 exports.PUNCTUATION_COLOR = '#555'
 exports.SUBTLE_COLOR = '#555E'
 
@@ -10,14 +12,16 @@ const allThemes = {
     '#480032',
     '#F25F25',
     '#d60257',
-    '#089fbf',
+    // '#089fbf',
+    '#0081f1',
   ],
   DancingDays   : [ '#c07B00', '#4D9375', '#7A7A7A', PURPLE, '#cd526c' ],
   FunkyDrummer  : [ '#404467', '#2a814d', '#0a0a0a', DARK_YELLOW, '#CA7081' ],
   GlassOnion    : [ '#f17048', BLUE, '#360745', '#953b69', '#ae0c3e' ],
   HelloSpaceboy : [ PURPLE, '#3b0032', '#38978D', BLUE, '#d12771' ],
   KozmicBlues   : [ '#218d54', `#d12771`, '#1b66ff', '#7d144c', DARK_YELLOW ],
-  LedZeppelin   : [ '#3a0088', '#d239a0', '#aa9132', '#2ea2a1', '#480656' ],
+  LedZeppelin   : [ '#3a0088', '#bd4b00', '#aa9132', '#5bb98c', '#480656' ],
+  // LedZeppelin   : [ '#3a0088', '#d239a0', '#aa9132', '#2ea2a1', '#480656' ],
   StrangeBrew   : [ '#222d60', '#01676b', '#fb607f', '#1b66ff', DARK_YELLOW ],
   SweatLeaf     : [ BLUE, '#2f586f', DARK_YELLOW, '#F92672', '#85005b' ],
 }
