6d5208ac366aba1a1f79f8ca8f5782472d1b2e29
Deyan Totev <deyan.totev.extern@seven.one>
2023-10-19 09:38:49 +0300

    chore@prepublish

--- src/assets/themes-colors.js
+++ src/assets/themes-colors.js
@@ -7,21 +7,56 @@ exports.PUNCTUATION_COLOR = '#555'
 exports.SUBTLE_COLOR = '#555E'
 
 const allThemes = {
-  CommunicationBreakdown : [
-    `#089fbf`,
+  CommunicationBreakdown: [
+    // `#089fbf`,
+    `#0093A1`,
     '#480032',
     '#F25F25',
     '#d60257',
-    '#0081f1',
+    '#0F54D6',
+    // '#0081f1',
   ],
-  DancingDays   : [ '#c07B00', '#4D9375', '#7A7A7A', PURPLE, '#cd526c' ],
-  FunkyDrummer  : [ '#404467', '#2a814d', '#0a0a0a', DARK_YELLOW, '#CA7081' ],
-  GlassOnion    : [ '#f17048', BLUE, '#360745', '#953b69', '#ae0c3e' ],
-  HelloSpaceboy : [ PURPLE, '#3b0032', '#38978D', BLUE, '#d12771' ],
-  KozmicBlues   : [ '#218d54', `#d12771`, DARK_YELLOW , '#7d144c',  DARK],
-  LedZeppelin   : [ '#3a0088', '#bd4b00', '#aa9132', '#5bb98c', '#480656' ],
-  StrangeBrew   : [ '#222d60', '#01676b', '#fb607f', '#1b66ff', DARK_YELLOW ],
-  SweatLeaf     : [ DARK, '#2f586f', DARK_YELLOW, '#F92672', '#85005b' ],
+  DancingDays: [
+    // '#c07B00',
+    '#8C6C41',
+    '#4D9375',
+    '#7A7A7A',
+    PURPLE,
+    '#cd526c',
+  ],
+  FunkyDrummer: [
+    '#404467',
+    '#2a814d',
+    '#0a0a0a',
+    DARK_YELLOW,
+    '#CA7081',
+  ],
+  GlassOnion: ['#f17048', BLUE, '#360745', '#953b69', '#ae0c3e'],
+  HelloSpaceboy: [
+    PURPLE,
+    '#383838',
+    // '#3b0032',
+    '#38978D',
+    BLUE,
+    '#d12771',
+  ],
+  KozmicBlues: ['#218d54', `#d12771`, DARK_YELLOW, '#7d144c', DARK],
+  LedZeppelin: [
+    // '#3a0088',
+    '#6B2FBA',
+    '#bd4b00',
+    '#aa9132',
+    '#5bb98c',
+    '#480656',
+  ],
+  StrangeBrew: [
+    '#222d60',
+    '#01676b',
+    '#fb607f',
+    '#1b66ff',
+    DARK_YELLOW,
+  ],
+  SweatLeaf: [DARK, '#2f586f', DARK_YELLOW, '#F92672', '#85005b'],
 }
 
 exports.allThemes = allThemes
