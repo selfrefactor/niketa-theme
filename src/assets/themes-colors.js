7369296662e416d54bd43740c5cae55dfa5af416
Deyan Totev <deyan.totev.extern@seven.one>
2023-11-15 08:10:34 +0200

    chore@prepublish

--- src/assets/themes-colors.js
+++ src/assets/themes-colors.js
@@ -1,22 +1,25 @@
-const PURPLE = '#774499'
+// const PURPLE = '#774499'
+const PURPLE = '#6B2FBA'
 // const DARK_YELLOW = '#ff6369'
-const DARK_YELLOW = '#C27D40'
-const BLUE = '#1b66ff'
-let DARK = '#433'
+// const DARK_YELLOW = '#C27D40'
+const DARK_YELLOW = '#aa9132'
+const BLUE = '#0081f1'
+// const BLUE = '#1b66ff'
+const DARK = '#433'
 exports.PUNCTUATION_COLOR = '#555'
 exports.SUBTLE_COLOR = '#555E'
 
 const allThemes = {
-  CommunicationBreakdown: [
+  CommunicationBreakdown : [
     // `#089fbf`,
-    `#0093A1`,
+    '#0093A1',
     '#480032',
     '#F25F25',
     '#d60257',
     '#0F54D6',
     // '#0081f1',
   ],
-  DancingDays: [
+  DancingDays : [
     // '#c07B00',
     '#8C6C41',
     '#4D9375',
@@ -24,15 +27,16 @@ const allThemes = {
     PURPLE,
     '#cd526c',
   ],
-  FunkyDrummer: [
-    '#404467',
-    '#2a814d',
-    '#0a0a0a',
-    DARK_YELLOW,
-    '#CA7081',
+  FunkyDrummer : [ '#404467', '#2a814d', '#0a0a0a', DARK_YELLOW, '#CA7081' ],
+  GlassOnion   : [
+    '#f17048',
+    BLUE,
+    '#360745',
+    // '#953b69',
+    '#A44185',
+    '#ae0c3e',
   ],
-  GlassOnion: ['#f17048', BLUE, '#360745', '#953b69', '#ae0c3e'],
-  HelloSpaceboy: [
+  HelloSpaceboy : [
     PURPLE,
     '#383838',
     // '#3b0032',
@@ -40,23 +44,18 @@ const allThemes = {
     BLUE,
     '#d12771',
   ],
-  KozmicBlues: ['#218d54', `#d12771`, DARK_YELLOW, '#7d144c', DARK],
-  LedZeppelin: [
+  KozmicBlues : [ '#218d54', '#d12771', DARK_YELLOW, '#7d144c', DARK ],
+  LedZeppelin : [
     // '#3a0088',
-    '#6B2FBA',
+    // '#6B2FBA',
+    '#2F86D2',
     '#bd4b00',
     '#aa9132',
     '#5bb98c',
     '#480656',
   ],
-  StrangeBrew: [
-    '#222d60',
-    '#01676b',
-    '#fb607f',
-    '#1b66ff',
-    DARK_YELLOW,
-  ],
-  SweatLeaf: [DARK, '#2f586f', DARK_YELLOW, '#F92672', '#85005b'],
+  StrangeBrew : [ '#222d60', '#01676b', '#fb607f', '#1b66ff', DARK_YELLOW ],
+  SweatLeaf   : [ DARK, '#2f586f', DARK_YELLOW, '#F92672', '#85005b' ],
 }
 
 exports.allThemes = allThemes
